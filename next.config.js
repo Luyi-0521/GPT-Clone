2ae5573ccb76ff733713979ccc429ab8aa3c4e72
Luyi-0521 <1062181469@qq.com>
2026-04-20 23:41:36 +0800

    Deploy GPT Clone application without README

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // 其他配置...
-}
-
-module.exports = nextConfig
---
+++ out/_next/static/sfSkfdmMiUz22JMgPvSsI/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0a7978991fe0777b.js"
+  ],
+  "/_error": [
+    "static/chunks/7f4b869c45408ed7.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
