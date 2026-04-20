8158ece1beec5f411af5bbeeccc9838ba8303786
Luyi-0521 <Luyi-0521@users.noreply.github.com>
2026-04-20 16:13:26 +0000

    deploy: 079ba2d08d8dec4b98f5c911ee271c137b29de02

--- out/_next/static/chunks/0a7978991fe0777b.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/e4d02a4fb63c9688.js",
-  "static/chunks/8860dc25068d3e1f.js",
-  "static/chunks/turbopack-79eff537de45bd8a.js"
-])
--- out/_next/static/chunks/1b9d50584f2c1268.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/ea52d10367cad89e.js",
-  "static/chunks/8860dc25068d3e1f.js",
-  "static/chunks/e67280da7d622211.css",
-  "static/chunks/turbopack-ec3199056ca25e88.js"
-])
